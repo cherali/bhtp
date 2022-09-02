3d46a4e703c24a6fec7f5b687e7193d0d16ecbc1
alich6 <alicheraghi148@gmail.com>
2022-09-02 04:51:23 +0430

    add lang and dir

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 function Document() {
   return (
-    <Html>
+    <Html lang='en' dir='ltr'>
       <Head>
         <link href='https://fonts.googleapis.com/css?family=Nunito&display=swap' rel='stylesheet' />
       </Head>
