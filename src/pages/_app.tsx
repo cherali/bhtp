0c591b3743507b760ce3d3d62bc84bd04c2d4bd5
alich6 <alicheraghi148@gmail.com>
2022-09-02 04:51:06 +0430

    add reset style and font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import { Provider } from 'react-redux'
 import EmotionCacheProvider from 'theme/EmotionCacheProvider'
 import ThemeProvider from 'theme/ThemeProvider'
 import { store } from 'store'
+import MainStyles from 'theme/MainStyles'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <EmotionCacheProvider>
         <ThemeProvider>
+          <MainStyles />
           <Component {...pageProps} />
         </ThemeProvider>
       </EmotionCacheProvider>
---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href='https://fonts.googleapis.com/css?family=Nunito&display=swap' rel='stylesheet' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Document
